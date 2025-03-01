13cd6bacced6e39af8b12c37977374bd5013b18b
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-03-01 11:30:45 -0600

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+  function superbowlWin(array) {
+    const winner = array.find(game => game.result === 'W');
+    if (winner) {
+        return winner.year;
+    } else {
+        return undefined;
+    }
+  }
+  
+  superbowlWin(record)
